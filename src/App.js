67c9c7d5ff35dad6832282168c94967ba2a6c0a5
Edmond <es27651@seeu.edu.mk>
2021-10-13 09:28:45 +0200

    Reply Component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Stack } from "@chakra-ui/layout";
+import Reply from "./components/Reply";
+
+
+function App() {
+  return (
+    <Stack>
+      <Reply /> 
+    </Stack>
+  
+  );
+}
+
+export default App;
